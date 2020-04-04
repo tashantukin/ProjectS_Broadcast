0006891c703f18db3f1881fc74566af4448e70c7
Codemafia <32629251+tashantukin@users.noreply.github.com>
2020-04-05 07:50:23 +0800

    Revert "Pushed Everything"
    This reverts commit 5112cbd13c43f007974935195da5c253390e7f99.

--- ajax/betOddsWala.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once('../db.php'); 
-        $sql = "SELECT value as oddsWala FROM settings where ID = 3";
-        $resultset = mysqli_query($conn, $sql); //or die("database error:". mysqli_error($conn));  
-        while( $record = mysqli_fetch_assoc($resultset)) {
-            echo number_format($record['oddsWala']);
-        }
- ?>
